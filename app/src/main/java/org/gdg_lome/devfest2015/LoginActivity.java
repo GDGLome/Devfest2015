7e2be5cfc0575787fc3b7513d9fd2de0e432e4e8
setico <seticoco@gmail.com>
2015-10-11 13:46:55 +0000

    Add Eventbrite confirmation code scan capability :)

--- app/src/main/java/org/gdg_lome/devfest2015/LoginActivity.java
+++ app/src/main/java/org/gdg_lome/devfest2015/LoginActivity.java
@@ -4,15 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+
+import org.gdg_lome.devfest2015.scanner.ScannerActivity;
 
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText tf_confirmation_code;
+    private String mScanFormat = "Format:";
+    private String mScanContents = "Contents:";
+    private final String BARCODE="barcode";
+    private final int SCANNER_ID=100;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        tf_confirmation_code = (EditText) findViewById(R.id.confirmation_code);
 
     }
 
@@ -20,9 +29,23 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(new Intent(this, MainActivity.class));
     }
 
-    public void register(View v){
-        startActivity(new Intent(this,MainActivity.class));
+    public void scan(View v){
+        startActivityForResult(new Intent(this, ScannerActivity.class), SCANNER_ID);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode) {
+            case SCANNER_ID:
+                if (resultCode == RESULT_OK) {
+                    Bundle res = data.getExtras();
+                    tf_confirmation_code.setText(res.getString(BARCODE));
+                }
+                break;
+        }
+    }
+
 
 
 }
---
+++ app/src/main/java/org/gdg_lome/devfest2015/scanner/ScannerActivity.java
@@ -0,0 +1,52 @@
+package org.gdg_lome.devfest2015.scanner;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class ScannerActivity extends Activity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView mScannerView;
+    private final String BARCODE="barcode";
+
+    @Override
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+        mScannerView = new ZXingScannerView(this);   // Programmatically initialize the scanner view
+        setContentView(mScannerView);                // Set the scanner view as the content view
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        mScannerView.startCamera();          // Start camera on resume
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();           // Stop camera on pause
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        // Do something with the result here
+        finishWithResult(rawResult.getText());
+    }
+
+
+    private void finishWithResult(String barcode)
+    {
+        Bundle conData = new Bundle();
+        conData.putString(BARCODE,barcode);
+        Intent intent = new Intent();
+        intent.putExtras(conData);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+}
